e33b19fde23d737ea7b5708e6fdce3a4ac7ca5ac
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-02-05 18:17:03 +0530

    CORS_ALLOWED_ORIGINS added

--- ai_bot/settings.py
+++ ai_bot/settings.py
@@ -138,5 +138,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    
+    "http://hybrid.srishticampus.in/lexsi/",
+    "localhost:5174",
+    "localhost:5175",
 ]
